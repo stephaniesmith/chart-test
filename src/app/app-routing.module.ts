84aee42decb74e00759f5037be7970fafb188fd0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-06 09:58:26 -0800

    bar chart! 📊🚀

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bar-chart', component: MyBarChartComponent },
+  { path: '**', component: MyBarChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-bar-chart',
+  templateUrl: './my-bar-chart.component.html',
+  styleUrls: ['./my-bar-chart.component.scss']
+})
+export class MyBarChartComponent implements OnInit {
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    { data: [65, 34, 23, 29, 87, 75, 28], label: 'Series A' },
+    { data: [32, 19, 63, 74, 48, 22, 64], label: 'Series B' },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
